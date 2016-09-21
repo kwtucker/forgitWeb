c0eacbb714250478a075f2e322fc6438f4318d1c
Kevin Tucker <kwtucker4@gmail.com>
2016-09-21 08:14:50 -0400

    imported mongo connection and passed it to base controller

--- routers/router.go
+++ routers/router.go
@@ -3,15 +3,15 @@ package routers
 import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/kwtucker/forgit/controllers"
+	"github.com/kwtucker/forgit/db"
 	"github.com/kwtucker/forgit/system"
 	"net/http"
 )
 
 // Init ...
-func Init(application system.Application, router *httprouter.Router) {
+func Init(application system.Application, router *httprouter.Router, database *db.ConnectMongo) {
 	// Serve static files
 	router.ServeFiles("/static/*filepath", http.Dir("static"))
-
 	// Routes
-	router.GET("/", application.Route(&controllers.IndexController{}, "Index"))
+	router.GET("/", application.Route(&controllers.IndexController{Env: application, DataConnect: database}, "Index"))
 }
