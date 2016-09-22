cd183c840b39db8c843b3d858a25558d993b8469
Kevin Tucker <kwtucker4@gmail.com>
2016-09-22 09:07:19 -0400

    when passing the Session struct I uppercased it so it would be exportable

--- routers/router.go
+++ routers/router.go
@@ -5,6 +5,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/kwtucker/forgit/controllers"
 	"github.com/kwtucker/forgit/db"
+	"github.com/kwtucker/forgit/lib"
 	"github.com/kwtucker/forgit/system"
 	"net/http"
 )
@@ -13,11 +14,14 @@ import (
 func Init(application system.Application, router *httprouter.Router, database *db.ConnectMongo) {
 	var sessionSecret = application.Config.SessionSecret
 	var Store = sessions.NewCookieStore([]byte(sessionSecret))
+
 	// Serve static files
 	router.ServeFiles("/static/*filepath", http.Dir("static"))
 
 	// Routes
-	router.GET("/", application.Route(&controllers.IndexController{Env: application, DataConnect: database, sess: Store}, "Index"))
+	router.GET("/", application.Route(&controllers.IndexController{Env: application, DataConnect: database, Sess: Store}, "Index"))
 	router.GET("/auth/", application.NoViewRoute(&lib.Auth{Env: application}, "AuthFunc"))
-	router.GET("/terminal/", application.Route(&controllers.TerminalController{Env: application, sess: Store}, "Terminal"))
+	router.GET("/auth/callback/", application.NoViewRoute(&lib.Auth{Env: application, Sess: Store}, "Callback"))
+	router.GET("/terminal/", application.Route(&controllers.TerminalController{Sess: Store, Env: application}, "Terminal"))
+	router.GET("/logout", application.NoViewRoute(&lib.Logout{Sess: Store, Env: application}, "Logout"))
 }
