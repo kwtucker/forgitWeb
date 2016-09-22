709dda0808ef5a0ab2feaeb8be641577bcc997b1
Kevin Tucker <kwtucker4@gmail.com>
2016-09-22 08:53:30 -0400

    fixed the session problem. Now they can't access the terminal without being logged in.

--- controllers/terminal.go
+++ controllers/terminal.go
@@ -1,43 +1,51 @@
 package controllers
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
-	"github.com/google/go-github/github"
+	// "github.com/google/go-github/github"
 	"github.com/gorilla/sessions"
 	"github.com/julienschmidt/httprouter"
-	"github.com/kwtucker/forgit_noFrame/models"
-	"github.com/kwtucker/forgit_noFrame/system"
-	"golang.org/x/oauth2"
-	"io/ioutil"
-	"log"
+	// "github.com/kwtucker/forgit_noFrame/models"
+	"github.com/kwtucker/forgit/system"
+	// "golang.org/x/oauth2"
+	// "io/ioutil"
+	// "log"
 	"net/http"
 )
 
 // TerminalController ...
 type TerminalController struct {
 	Env  system.Application
-	sess *sessions.CookieStore
+	Sess *sessions.CookieStore
 }
 
 // Terminal ...
 func (c *TerminalController) Terminal(w http.ResponseWriter, r *http.Request, ps httprouter.Params) (map[string]interface{}, int) {
 	// Grab the Session
-	session, err := c.sess.Get(r, "ForgitSession")
+	session, err := c.Sess.Get(r, "ForgitSession")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	// Get values from URL
-	code := r.FormValue("code")
-	state := r.FormValue("state")
 
-	// Check if github returned the same state that I made the request with.
-	if state != c.Env.Config.GithubState {
+	// If the person is not authed send them back to home
+	if session.Values["authed"] != 1 {
 		session.Values["authed"] = 0
+		fmt.Println("authed not term", session.Values["authed"])
 		session.Save(r, w)
-		http.Redirect(w, r, "/", http.StatusUnauthorized)
-	} else {
-		// logged in
-		session.Values["authed"] = 1
+		http.Redirect(w, r, "http://"+c.Env.Config.HostString()+"/", http.StatusFound)
 	}
+	// Nav for this view.
+	navLinks := map[string]string{
+		"/terminal/":      "Terminal",
+		"#gettingstarted": "Getting Started",
+		"/logout":         "Logout",
+	}
+	// data for the view
+	data := map[string]interface{}{
+		"PageName":        "Terminal",
+		"ContentTemplate": "terminal",
+		"NavLinks":        navLinks,
+	}
+	return data, http.StatusOK
 }
