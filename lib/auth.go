c63e4c50a5d56325cac653cbf9e0f066eac52916
Kevin Tucker <kwtucker4@gmail.com>
2016-09-22 15:03:50 -0400

    added a session checker and a logout functin that does exactly what the names imply

--- lib/auth.go
+++ lib/auth.go
@@ -1,7 +1,6 @@
 package lib
 
 import (
-	"fmt"
 	"github.com/gorilla/sessions"
 	"github.com/julienschmidt/httprouter"
 	"github.com/kwtucker/forgit/system"
@@ -34,11 +33,38 @@ func (a *Auth) Callback(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 		http.Redirect(w, r, "/", http.StatusUnauthorized)
 	} else {
 		// logged in
-		fmt.Println("authed")
 		session.Values["authed"] = 1
+		session.Values["code"] = code
 		session.Save(r, w)
-		fmt.Println("authed", session.Values["authed"])
-		fmt.Println("http://" + a.Env.Config.HostString() + "/terminal/")
 		http.Redirect(w, r, "/terminal/", http.StatusFound)
 	}
+}
+
+// SessionCheck ...
+func (a *Auth) SessionCheck(w http.ResponseWriter, r *http.Request) *sessions.Session {
+	// Grab the Session
+	session, err := a.Sess.Get(r, "ForgitSession")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// If the person is not authed send them back to home
+	if session.Values["authed"] != 1 {
+		session.Values["authed"] = 0
+		session.Save(r, w)
+		http.Redirect(w, r, "http://"+a.Env.Config.HostString()+"/", http.StatusFound)
+	}
+	return session
+}
+
+// Logout will handle the event of clicking the logout button.
+func (a *Auth) Logout(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Grab the Session
+	session, err := a.Sess.Get(r, "ForgitSession")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// set auth to 0 for not authed and save it. Then send user home.
+	session.Values["authed"] = 0
+	session.Save(r, w)
+	http.Redirect(w, r, "http://"+a.Env.Config.HostString()+"/", http.StatusFound)
 }
