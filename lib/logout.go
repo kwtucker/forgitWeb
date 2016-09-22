edaf932160e0a93293934dad8e2c3c2782ecd481
Kevin Tucker <kwtucker4@gmail.com>
2016-09-22 09:00:20 -0400

    updated comments

--- lib/logout.go
+++ lib/logout.go
@@ -13,13 +13,15 @@ type Logout struct {
 	Sess *sessions.CookieStore
 }
 
-// Logout ...
+// Logout will handle the event of clicking the logout button.
 func (l *Logout) Logout(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// Grab the Session
 	session, err := l.Sess.Get(r, "ForgitSession")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	// set auth to 0 for not authed and save it. Then send user home.
 	session.Values["authed"] = 0
 	session.Save(r, w)
 	http.Redirect(w, r, "http://"+l.Env.Config.HostString()+"/", http.StatusFound)
