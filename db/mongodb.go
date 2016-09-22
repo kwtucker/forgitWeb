d6472de31a1d38c6d6e8f080f0feab6114184375
Kevin Tucker <kwtucker4@gmail.com>
2016-09-22 15:02:09 -0400

    added a AddUser function that will insert a user to the db

--- db/mongodb.go
+++ db/mongodb.go
@@ -1,9 +1,13 @@
 package db
 
 import (
-	// "github.com/kwtucker/forgit/models"
+	"fmt"
+	"github.com/google/go-github/github"
+	"github.com/kwtucker/forgit/models"
 	"gopkg.in/mgo.v2"
-	// "gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"time"
 )
 
 // ConnectMongo ...
@@ -20,6 +24,70 @@ func (c *ConnectMongo) ConnectDB(dburl, dbname string) {
 	}
 	c.DBSession = session
 	c.DName = session.DB(dbname)
+}
+
+// AddUser ...
+func (c *ConnectMongo) AddUser(dbCopy *ConnectMongo, client *github.Client) {
+
+	// Get logged in user
+	user, _, err := client.Users.Get("")
+	if err != nil {
+		log.Println(err)
+	}
+	repos, _, err := client.Repositories.List("", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var repoArr = []models.Repo{}
+	for k, _ := range repos {
+		currentUserRepos := models.Repo{
+			URL:             repos[k].URL,
+			CommitsURL:      repos[k].CommitsURL,
+			ContributorsURL: repos[k].ContributorsURL,
+			Description:     repos[k].Description,
+			FullName:        repos[k].FullName,
+			GitCommitsURL:   repos[k].GitCommitsURL,
+			HTMLURL:         repos[k].HTMLURL,
+			ID:              repos[k].ID,
+			Name:            repos[k].Name,
+			Owner:           repos[k].Owner.Login,
+		}
+		repoArr = append(repoArr, currentUserRepos)
+	}
+	timenow := &github.Timestamp{time.Now()}
+	currentUser := &models.User{
+		ID:         user.ID,
+		LastUpdate: timenow.String(),
+		LastSync:   timenow.String(),
+		Login:      user.Login,
+		Name:       user.Name,
+		AvatarURL:  user.AvatarURL,
+		Company:    user.Company,
+		HTMLURL:    user.HTMLURL,
+		ReposURL:   user.ReposURL,
+		Repos:      repoArr,
+		// Settings:   []Setting,
+	}
+
+	// select the db and collection to use
+	d := dbCopy.DBSession.DB("forgit").C("users")
+
+	// Insert and handle error if any
+	err = d.Insert(currentUser)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// find one in db and set to struct
+	result := models.User{}
+	err = d.Find(bson.M{"login": "kwtucker"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("User Full Name:", result.Name)
+
 }
 
 // FindOne ..
