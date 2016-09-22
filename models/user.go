16ecf2c9b19d791af2ee69f5723c701d87e04a13
Kevin Tucker <kwtucker4@gmail.com>
2016-09-22 17:25:52 -0400

    added bson for mongo

--- models/user.go
+++ models/user.go
@@ -2,38 +2,38 @@ package models
 
 // User ...
 type User struct {
-	ID         *int      `json:"id"`
-	LastUpdate string    `json:"lastUpdate"`
-	LastSync   string    `json:"lastSync"`
-	Login      *string   `json:"login"`
-	Name       *string   `json:"name"`
-	AvatarURL  *string   `json:"avatar_url"`
-	Company    *string   `json:"company"`
-	HTMLURL    *string   `json:"html_url"`
-	ReposURL   *string   `json:"repos_url"`
-	Repos      []Repo    `json:"repos"`
-	Settings   []Setting `json:"settings"`
+	ID         *int      `bson:"githubID"`
+	LastUpdate string    `bson:"lastUpdate"`
+	LastSync   string    `bson:"lastSync"`
+	Login      *string   `bson:"login"`
+	Name       *string   `bson:"name"`
+	AvatarURL  *string   `bson:"avatar_url"`
+	Company    *string   `bson:"company"`
+	HTMLURL    *string   `bson:"html_url"`
+	ReposURL   *string   `bson:"repos_url"`
+	Repos      []Repo    `bson:"repos"`
+	Settings   []Setting `bson:"settings"`
 }
 
 // Repo ...
 type Repo struct {
-	URL             *string `json:"url"`
-	CommitsURL      *string `json:"commits_url"`
-	ContributorsURL *string `json:"contributors_url"`
-	Description     *string `json:"description"`
-	FullName        *string `json:"full_name"`
-	GitCommitsURL   *string `json:"git_commits_url"`
-	HTMLURL         *string `json:"html_url"`
-	ID              *int    `json:"id"`
-	Name            *string `json:"name"`
-	Owner           *string `json:"owner"`
+	URL             *string `bson:"url"`
+	CommitsURL      *string `bson:"commits_url"`
+	ContributorsURL *string `bson:"contributors_url"`
+	Description     *string `bson:"description"`
+	FullName        *string `bson:"full_name"`
+	GitCommitsURL   *string `bson:"git_commits_url"`
+	HTMLURL         *string `bson:"html_url"`
+	ID              *int    `bson:"repo_id"`
+	Name            *string `bson:"name"`
+	Owner           *string `bson:"owner"`
 }
 
 // Setting ...
 type Setting struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Status int    `json:"status"`
+	ID     int    `bson:"id"`
+	Name   string `bson:"name"`
+	Status int    `bson:"status"`
 	SettingNotifications
 	SettingAddPullCommit
 	SettingPush
@@ -42,27 +42,27 @@ type Setting struct {
 
 // SettingNotifications ...
 type SettingNotifications struct {
-	Status   int `json:"status"`
-	OnError  int `json:"onError"`
-	OnCommit int `json:"onCommit"`
-	OnPush   int `json:"onPush"`
+	Status   int `bson:"status"`
+	OnError  int `bson:"onError"`
+	OnCommit int `bson:"onCommit"`
+	OnPush   int `bson:"onPush"`
 }
 
 // SettingAddPullCommit ...
 type SettingAddPullCommit struct {
-	Status  int `json:"status"`
-	TimeMin int `json:"timeMinute"`
+	Status  int `bson:"status"`
+	TimeMin int `bson:"timeMinute"`
 }
 
 // SettingPush ...
 type SettingPush struct {
-	Status  int `json:"status"`
-	TimeMin int `json:"timeMinute"`
+	Status  int `bson:"status"`
+	TimeMin int `bson:"timeMinute"`
 }
 
 // SettingRepo ...
 type SettingRepo struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Status int    `json:"status"`
+	ID     int    `bson:"id"`
+	Name   string `bson:"name"`
+	Status int    `bson:"status"`
 }
