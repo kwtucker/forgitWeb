a57a94868f0a6a399254dc1b16a445f960f7146c
Kevin Tucker <kwtucker4@gmail.com>
2016-09-23 16:32:47 -0400

    added some validation of the user logged in. so the you can refresh the page without error

--- controllers/terminal.go
+++ controllers/terminal.go
@@ -1,18 +1,15 @@
 package controllers
 
 import (
-	// "encoding/json"
 	"fmt"
 	"github.com/google/go-github/github"
 	"github.com/gorilla/sessions"
 	"github.com/julienschmidt/httprouter"
 	"github.com/kwtucker/forgit/db"
 	"github.com/kwtucker/forgit/lib"
-	// "github.com/kwtucker/forgit/models"
 	"github.com/kwtucker/forgit/system"
 	"golang.org/x/oauth2"
-	// "io/ioutil"
-	// "log"
+	"log"
 	"net/http"
 )
 
@@ -31,24 +28,60 @@ func (c *TerminalController) Connect() *db.ConnectMongo {
 
 // Terminal ...
 func (c *TerminalController) Terminal(w http.ResponseWriter, r *http.Request, ps httprouter.Params) (map[string]interface{}, int) {
-	// This will check if user is authed
-	var isAuth = &lib.Auth{Sess: c.Sess, Env: c.Env}
-
-	// validate the session and get the session back
-	session := isAuth.SessionCheck(w, r)
-
-	// get access token and get client.
-	tok, _ := c.Env.AuthConf.Exchange(oauth2.NoContext, session.Values["code"].(string))
-	tokc := c.Env.AuthConf.Client(oauth2.NoContext, tok)
-	client := github.NewClient(tokc)
-	fmt.Println(client.Octocat("Oh JUMMMMMM"))
-
 	// copy db pipeline and
 	// don't close session tell end of function
 	dbconnect := c.Connect()
 	defer dbconnect.DBSession.Close()
 
-	c.db.AddUser(dbconnect, client)
+	// This will check if user is authed
+	var isAuth = &lib.Auth{Sess: c.Sess, Env: c.Env}
+
+	// validate the session and get the session back
+	// if request not valid redirected to "/""
+	session := isAuth.SessionCheck(w, r)
+
+	// If the user id is set the user is logged in
+	if session.Values["userID"] == nil {
+		tok, _ := c.Env.AuthConf.Exchange(oauth2.NoContext, session.Values["code"].(string))
+		tokc := c.Env.AuthConf.Client(oauth2.NoContext, tok)
+		client := github.NewClient(tokc)
+		fmt.Println(client.Octocat("Oh JUMMMMMM"))
+
+		// Get logged in user
+		//[]*Repository, *Response, error
+		ghuser, _, err := client.Users.Get("")
+		if err != nil {
+			log.Println(err)
+		}
+		session.Values["userID"] = *ghuser.ID
+		session.Save(r, w)
+
+		// Get logged in users repos
+		//[]*Repository, *Response, error
+		repos, _, err := client.Repositories.List("", nil)
+		if err != nil {
+			log.Println(err)
+		}
+		//
+		CheckUserExists, err := c.db.Exists(dbconnect, ghuser.ID)
+		if err != nil {
+			log.Println(err)
+		}
+
+		switch CheckUserExists {
+		case false:
+			User := lib.CreateUser(ghuser, repos)
+			err = c.db.AddUser(dbconnect, User)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
+	User, err := c.db.FindOneUser(dbconnect, session.Values["userID"].(int))
+	if err != nil {
+		log.Println(err)
+	}
 
 	// Nav for this view.
 	navLinks := map[string]string{
@@ -62,6 +95,7 @@ func (c *TerminalController) Terminal(w http.ResponseWriter, r *http.Request, ps
 		"PageName":        "Terminal",
 		"ContentTemplate": "terminal",
 		"NavLinks":        navLinks,
+		"User":            User,
 	}
 	return data, http.StatusOK
 }
