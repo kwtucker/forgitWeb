e4aa34c349777ba0d787d578437f44295cd5951d
Kevin Tucker <kwtucker4@gmail.com>
2016-09-25 11:58:24 -0400

    updated the logout function to remove the cached token, and remove the session check for now, and added in token set to callback

--- lib/auth.go
+++ lib/auth.go
@@ -1,9 +1,11 @@
 package lib
 
 import (
+	"fmt"
 	"github.com/gorilla/sessions"
 	"github.com/julienschmidt/httprouter"
 	"github.com/kwtucker/forgit/system"
+	"golang.org/x/oauth2"
 	"net/http"
 )
 
@@ -32,28 +34,18 @@ func (a *Auth) Callback(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 	if code == "" || state == "" || state != a.Env.Config.GithubState {
 		http.Redirect(w, r, "/", http.StatusUnauthorized)
 	} else {
-		// logged in
+		tok, err := a.Env.AuthConf.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Save authed values
+		session.Values["token"] = tok.AccessToken
 		session.Values["authed"] = 1
-		session.Values["code"] = code
 		session.Save(r, w)
+
 		http.Redirect(w, r, "/terminal/", http.StatusFound)
 	}
-}
-
-// SessionCheck ...
-func (a *Auth) SessionCheck(w http.ResponseWriter, r *http.Request) *sessions.Session {
-	// Grab the Session
-	Session, err := a.Sess.Get(r, "ForgitSession")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	// If the person is not authed send them back to home
-	if Session.Values["authed"] != 1 {
-		Session.Values["authed"] = 0
-		Session.Save(r, w)
-		http.Redirect(w, r, "http://"+a.Env.Config.HostString()+"/", http.StatusFound)
-	}
-	return Session
 }
 
 // Logout will handle the event of clicking the logout button.
@@ -65,6 +57,7 @@ func (a *Auth) Logout(w http.ResponseWriter, r *http.Request, ps httprouter.Para
 	}
 	// set auth to 0 for not authed and save it. Then send user home.
 	session.Values["authed"] = 0
+	session.Values["token"] = nil
 	session.Save(r, w)
 	http.Redirect(w, r, "http://"+a.Env.Config.HostString()+"/", http.StatusFound)
 }
