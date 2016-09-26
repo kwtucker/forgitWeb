34d172b3dd9a9923ca64fb1afe7322b2af665ce6
Kevin Tucker <kwtucker4@gmail.com>
2016-09-26 14:37:50 -0400

    update on the comments and worked in a conditional to determine if create user is a update or new

--- lib/lib.go
+++ lib/lib.go
@@ -9,13 +9,15 @@ import (
 )
 
 // CreateUser ...
-func CreateUser(user *github.User, repos []github.Repository) *models.User {
+func CreateUser(user *github.User, repos []github.Repository, update []models.Setting) *models.User {
 	var (
-		repoArr      = []models.Repo{}
-		settingRepos = []models.SettingRepo{}
-		settings     = []models.Setting{}
+		repoArr             = []models.Repo{}
+		settingRepos        = []models.SettingRepo{}
+		settings            = []models.Setting{}
+		currentUserSettings = models.Setting{}
 	)
 
+	// loop over all repos and set struct
 	for k := range repos {
 		currentUserRepos := models.Repo{
 			URL:             repos[k].URL,
@@ -39,32 +41,66 @@ func CreateUser(user *github.User, repos []github.Repository) *models.User {
 		settingRepos = append(settingRepos, currentUserSettingsRepo)
 	}
 
-	currentUserSettings := models.Setting{
-		SettingID: 1,
-		Name:      "General",
-		Status:    1,
-		SettingNotifications: models.SettingNotifications{
-			Status:   1,
-			OnError:  1,
-			OnCommit: 1,
-			OnPush:   1,
-		},
-		SettingAddPullCommit: models.SettingAddPullCommit{
-			Status:  1,
-			TimeMin: 5,
-		},
-		SettingPush: models.SettingPush{
-			Status:  1,
-			TimeMin: 60,
-		},
-		Repos: settingRepos,
+	// if update is a value set setting struct
+	if update != nil {
+		for u := range update {
+			currentUserSettings = models.Setting{
+				SettingID: update[u].SettingID,
+				Name:      update[u].Name,
+				Status:    update[u].Status,
+				SettingNotifications: models.SettingNotifications{
+					Status:   update[u].SettingNotifications.Status,
+					OnError:  update[u].SettingNotifications.OnError,
+					OnCommit: update[u].SettingNotifications.OnCommit,
+					OnPush:   update[u].SettingNotifications.OnPush,
+				},
+				SettingAddPullCommit: models.SettingAddPullCommit{
+					Status:  update[u].SettingAddPullCommit.Status,
+					TimeMin: update[u].SettingAddPullCommit.TimeMin,
+				},
+				SettingPush: models.SettingPush{
+					Status:  update[u].SettingPush.Status,
+					TimeMin: update[u].SettingPush.TimeMin,
+				},
+				Repos: settingRepos,
+			}
+			settings = append(settings, currentUserSettings)
+		}
 	}
-	settings = append(settings, currentUserSettings)
+
+	// Update is nil means new user
+	// Create user setting defualts
+	if update == nil {
+		currentUserSettings = models.Setting{
+			SettingID: 1,
+			Name:      "General",
+			Status:    1,
+			SettingNotifications: models.SettingNotifications{
+				Status:   1,
+				OnError:  1,
+				OnCommit: 1,
+				OnPush:   1,
+			},
+			SettingAddPullCommit: models.SettingAddPullCommit{
+				Status:  1,
+				TimeMin: 5,
+			},
+			SettingPush: models.SettingPush{
+				Status:  1,
+				TimeMin: 60,
+			},
+			Repos: settingRepos,
+		}
+		settings = append(settings, currentUserSettings)
+	}
+
+	// set the time
 	location, err := time.LoadLocation("America/New_York")
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	// set time and build user struct
 	timenow := &github.Timestamp{time.Now().In(location)}
 	currentUser := &models.User{
 		GithubID:   *user.ID,
@@ -83,6 +119,7 @@ func CreateUser(user *github.User, repos []github.Repository) *models.User {
 	return currentUser
 }
 
+// GetTokenStruct will create a new token struct and return a pointer to it
 func GetTokenStruct(token string) *oauth2.Token {
 	// getting new
 	var tok = oauth2.Token{
