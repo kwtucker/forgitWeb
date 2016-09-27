40ab1a47a3b828ab719d7b6d8d59560d6edadad2
Kevin Tucker <kwtucker4@gmail.com>
2016-09-26 20:28:37 -0400

    added getting started function

--- controllers/gettingstarted.go
+++ controllers/gettingstarted.go
@@ -1,1 +1,29 @@
 package controllers
+
+import (
+	// "fmt"
+	// "github.com/google/go-github/github"
+	"github.com/gorilla/sessions"
+	// "github.com/julienschmidt/httprouter"
+	"github.com/kwtucker/forgit/db"
+	// "github.com/kwtucker/forgit/lib"
+	// "github.com/kwtucker/forgit/models"
+	"github.com/kwtucker/forgit/system"
+	// "golang.org/x/oauth2"
+	// "log"
+	// "net/http"
+	// "time"
+)
+
+// GettingStartedController ...
+type GettingStartedController struct {
+	Env         system.Application
+	Sess        *sessions.CookieStore
+	DataConnect *db.ConnectMongo
+	db          db.ConnectMongo
+}
+
+// GettingStarted ...
+// func (c *GettingStartedController) GettingStarted(w http.ResponseWriter, r *http.Request, ps httprouter.Params) (map[string]interface{}, int) {
+//
+// }
