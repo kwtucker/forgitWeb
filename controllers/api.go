451957ba596e19853a9c38157417228bbf3f6c99
Kevin Tucker <kwtucker4@gmail.com>
2016-09-28 23:13:53 -0400

    added logic commented out in api. Haven't go to do that yet

--- controllers/api.go
+++ controllers/api.go
@@ -22,3 +22,30 @@ type APIController struct {
 // func (c *APIController) API(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 //
 // }
+
+//
+//
+// -=-=-=-=-=-= Covert times -=-=-=-=-=
+// // Converts string Unix time stamp of UTC to int64
+// dD, err := strconv.ParseInt(dbUser.LastUpdate, 10, 64)
+// if err != nil {
+// 	fmt.Println(err)
+// }
+// // conver to time.Time for compare
+// dbUserUpdateDate := time.Unix(dD, 0)
+// fmt.Println(dbUserUpdateDate)
+// -=-=-=-=-=-= End Covert times -=-=-=-=-=
+
+//
+//
+// =-=-=-=-API LOGIC =-=-=-=-
+
+// if the local env update time is after the db update time, then take settings from local
+
+// call update on user settings to db
+
+// else send server user settings to the local env
+
+// respond with the server settings
+
+// =-=-=-=- END API LOGIC =-=-=-=-
