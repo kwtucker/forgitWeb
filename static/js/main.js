91842fb13224d689c30b8821d877223ad97a66ac
Kevin Tucker <kwtucker4@gmail.com>
2016-10-03 13:58:47 -0400

    feature section finished

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,19 @@
+var currentArticle = "tab_1"
+var currentTab = 1
 $(function() {
+  if ($(window).width() < 1000) {
+    $('#features article').css('display','flex')
+    $('#features article:even').addClass('even')
+    $('#features').css('background', '#4DC4AD')
+  } else {
+    $('#features article').hide()
+    $('#features article').removeClass('even')
+    $('#tabs a').removeClass('selectedtab')
+    $('#tabs a[data-tab='+currentTab+']').addClass('selectedtab')
+    $('.tab_'+currentTab).css('display','flex')
+    $('#features').css('background', '#5E696D')
+  }
+
     $("#tabs a").addClass('hov');
     $("#tabs a[data-tab='1']").removeClass('hov').addClass('selectedtab')
     $(".tab_1").css('display','flex');
@@ -14,5 +29,24 @@ $(function() {
       $(this).removeClass('hov');
       var tab = $(this).data('tab')
       $('.tab_'+tab).css('display','flex')
+      currentTab = tab
+      console.log(currentTab);
     })
+});
+
+
+$(window).resize(function() {
+  console.log($(window).width())
+  if ($(window).width() < 1000) {
+    $('#features article').css('display','flex')
+    $('#features article:even').addClass('even')
+    $('#features').css('background', '#4DC4AD')
+  } else {
+    $('#features article').hide()
+    $('#features article').removeClass('even')
+    $('#tabs a').removeClass('selectedtab')
+    $('#tabs a[data-tab='+currentTab+']').addClass('selectedtab')
+    $('.tab_'+currentTab).css('display','flex')
+    $('#features').css('background', '#5E696D')
+  }
 });
