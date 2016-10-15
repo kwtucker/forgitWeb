f80fc6739534ba41ed187c5110e78523494f951e
Kevin Tucker <kwtucker4@gmail.com>
2016-10-15 14:40:16 -0400

    started the docs

--- controllers/gettingstarted.go
+++ controllers/gettingstarted.go
@@ -4,14 +4,14 @@ import (
 	// "fmt"
 	// "github.com/google/go-github/github"
 	"github.com/gorilla/sessions"
-	// "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 	"github.com/kwtucker/forgit/db"
 	// "github.com/kwtucker/forgit/lib"
 	// "github.com/kwtucker/forgit/models"
 	"github.com/kwtucker/forgit/system"
 	// "golang.org/x/oauth2"
-	// "log"
-	// "net/http"
+	"log"
+	"net/http"
 	// "time"
 )
 
@@ -23,7 +23,35 @@ type GettingStartedController struct {
 	db          db.ConnectMongo
 }
 
+// Connect will make a new copy of the main mongodb connection.
+func (c *GettingStartedController) Connect() *db.ConnectMongo {
+	return &db.ConnectMongo{DBSession: c.DataConnect.DBSession.Copy(), DName: c.DataConnect.DName}
+}
+
 // GettingStarted ...
-// func (c *GettingStartedController) GettingStarted(w http.ResponseWriter, r *http.Request, ps httprouter.Params) (map[string]interface{}, int) {
-//
-// }
+func (c *GettingStartedController) GettingStarted(w http.ResponseWriter, r *http.Request, ps httprouter.Params) (map[string]interface{}, int) {
+	// Grab the Session
+	session, err := c.Sess.Get(r, "ForgitSession")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// Copy db pipeline and
+	dbconnect := c.Connect()
+	defer dbconnect.DBSession.Close()
+
+	// Grab most current user info
+	dbUser, err := c.db.FindOneUser(dbconnect, session.Values["userID"].(int))
+	if err != nil {
+		log.Println(err)
+	}
+
+	// data for the view
+	data := map[string]interface{}{
+		"Auth":            true,
+		"PageName":        "Getting Started",
+		"ContentTemplate": "gettingStarted",
+		"User":            dbUser,
+	}
+	return data, http.StatusOK
+}
